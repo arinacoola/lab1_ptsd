8e8327feb54ac9ab98d9b43cdd97e02ddd7d035d
arinacoola <a.zvarunchyk@gmail.com>
2025-05-26 19:37:33 +0300

    ладно

--- terraform/lambda/lambda_function.py
+++ terraform/lambda/lambda_function.py
@@ -1,33 +1,23 @@
-import os
 import boto3
 
 def lambda_handler(event, context):
-    print("EVENT:", event)
-    queue_url = os.environ.get("SQS_QUEUE_URL")
-    print("SQS_QUEUE_URL:", queue_url)
-    destination_bucket = 's3-finish'
     s3 = boto3.client('s3')
     sqs = boto3.client('sqs')
-    for record in event['Records']:
-        source_bucket = record['s3']['bucket']['name']
-        print("SOURCE BUCKET:", source_bucket)
-        s3_key = record['s3']['object']['key']
-        print("S3 KEY:", s3_key)
-        copy_source = {'Bucket': source_bucket, 'Key': s3_key}
-        try:
-            s3.copy_object(
-                Bucket=destination_bucket,
-                CopySource=copy_source,
-                Key=s3_key
-            )
-            print(f"Copied {s3_key} to {destination_bucket}")
-        except Exception as e:
-            print("Error copying object:", e)
-        try:
-            resp = sqs.send_message(
-                QueueUrl=queue_url,
-                MessageBody=f"Copied {s3_key} from {source_bucket} to {destination_bucket}"
-            )
-            print("SQS send_message response:", resp)
-        except Exception as e:
-            print("Error sending SQS message:", e)
+    
+    source_bucket = 's3-start'
+    destination_bucket = 's3-finish'
+    queue_url = 'http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/lambda-queue'  
+    
+    object_key = event['Records'][0]['s3']['object']['key']
+    copy_source = {'Bucket': source_bucket, 'Key': object_key}
+    
+    s3.copy_object(
+        CopySource=copy_source,
+        Bucket=destination_bucket,
+        Key=object_key
+    )
+    
+    sqs.send_message(
+        QueueUrl=queue_url,
+        MessageBody=f"Copied {object_key} from {source_bucket} to {destination_bucket}"
+    )
