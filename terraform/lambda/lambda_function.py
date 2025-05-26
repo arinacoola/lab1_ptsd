a53c1231332250032d1524dafef2160d3d8b396b
arinacoola <a.zvarunchyk@gmail.com>
2025-05-26 20:45:31 +0300

    вертаю старе

--- terraform/lambda/lambda_function.py
+++ terraform/lambda/lambda_function.py
@@ -1,19 +1,22 @@
 import os
 import boto3
 
-def lambda_handler(event, context):
-    s3 = boto3.client('s3', endpoint_url='http://localhost:4566')
-    
-    source_bucket = os.environ["S3_START"]
-    destination_bucket = os.environ["S3_FINISH"]
-    object_key = event["Records"][0]["s3"]["object"]["key"]
+s3 = boto3.client('s3')
+sqs = boto3.client('sqs')
 
-    copy_source = {
-        'Bucket': source_bucket,
-        'Key': object_key
-    }
-    s3.copy_object(
-        Bucket=destination_bucket,
-        Key=object_key,
-        CopySource=copy_source
-    )
+def lambda_handler(event, context):
+    destination_bucket = 's3-finish'
+    queue_url = os.environ["SQS_QUEUE_URL"]  
+    for record in event['Records']:
+        source_bucket = record['s3']['bucket']['name']
+        s3_key = record['s3']['object']['key']
+        copy_source = {'Bucket': source_bucket, 'Key': s3_key}
+        s3.copy_object(
+            Bucket=destination_bucket,
+            CopySource=copy_source,
+            Key=s3_key
+        )
+        resp = sqs.send_message(
+            QueueUrl=queue_url,
+            MessageBody=f"Copied {s3_key} from {source_bucket} to {destination_bucket}"
+        )
